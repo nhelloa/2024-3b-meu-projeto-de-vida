f4c2269333533654902d793b4b61a2f6506355bc
Helloa <160602656+nhelloa@users.noreply.github.com>
2024-04-09 08:09:04 -0300

    08/04

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const botoes = document.querySelectorAll('.botao')
 const textos = document.querySelectorAll('.aba-conteudo')
 
+const contadores = document.querySelectorAll('.contador')
+
+const tempoObjetivo01 = new Date ("2024-12-22T00:00:00")
+const tempoObjetivo02 = new Date ("2024-11-13T00:00:00")
+const tempoObjetivo03 = new Date ("2024-12-10T00:00:00")
+const tempoObjetivo04 = new Date ("2025-02-20T00:00:00")
+
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04]
+
+for (let i = 0; i<tempos.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i])
+}
+
 for(let i = 0; i<botoes.length; i++){
     botoes[i].onclick = function (){
 
@@ -13,4 +26,21 @@ for(let i = 0; i<botoes.length; i++){
         textos[i].classList.add('ativo')
     }
 
+}
+
+function calculaTempo (tempoObjetivo) {
+
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo-tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dia = Math.floor(horas/24);
+
+    segundos%= 60;
+    minutos%= 60;
+    horas%= 24;
+
+    return dia + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos"
 }
